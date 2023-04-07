536ce2be1a8e72f2241fa542a43e23e1e7cdecd5
DoctorSir <aidenemerling@gmail.com>
2023-04-07 08:24:42 -0400

    new branch, lab12 complete

--- src/lab12/episode.py
+++ src/lab12/episode.py
@@ -11,3 +11,23 @@ Note that observation/state is a tuple of the form (player1_health, player2_heal
 Action is simply the weapon selected by the player.
 Reward is the reward for the player for that turn.
 '''
+import sys
+from pathlib import Path
+sys.path.append(str((Path(__file__) / ".." / "..").resolve().absolute()))
+
+from lab11.turn_combat import Combat
+from lab11.pygame_combat import run_turn
+
+def run_episode(player1, player2):
+    currentGame = Combat()
+    values = []
+
+    # Main Game Loop
+    while not currentGame.gameOver:
+        reward = run_turn(currentGame, player1, player2)
+        state = (player1.health, player2.health)
+        action = player1.weapon
+
+        values.append((state, action, reward))
+    
+    return values
